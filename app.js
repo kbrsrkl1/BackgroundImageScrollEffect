2f612f2567bcf00f1b005e2743fd442f26d53c84
kbrsrkl1 <kubranur216@gmail.com>
2025-06-28 12:25:37 +0300

    commit

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const bgImageEl = document.getElementById("bg-image")
 
-
 window.addEventListener("scroll", () => {
     upgradeImage()
 })
 
-
 function upgradeImage() {
-    bgImageEl.style.opacity = 1 - window.pageYOffset / 1000
-    bgImageEl.style.backgroundSize = 160 - window.pageYOffset / 10 + "%"
+    bgImageEl.style.opacity = 1 - window.pageYOffset / 800
+    bgImageEl.style.backgroundSize = 170 - window.pageYOffset / 12 + "%"
 }
